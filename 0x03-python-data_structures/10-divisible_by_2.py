6a1f2f5bfea940358010e2bc10edfb858b204fb3
neneeye <neneeye@gmail.com>
2022-12-02 20:17:47 +0000

    all at once

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    div_ = []
+
+    for i in range(len(my_list)):
+
+        if my_list[i] % 2 == 0:
+
+            div_.append(True)
+        else:
+            div_.append(False)
+
+            return (div_)
